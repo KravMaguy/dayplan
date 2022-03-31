abebd15b730d80b5bb2c600c514da908cf53a270
krav maguy <flex4lease@gmail.com>
2022-03-31 01:57:58 -0500

    set center by geolocation

--- client/src/App.js
+++ client/src/App.js
@@ -3,54 +3,14 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PlacesAutoComplete from "./PlacesAutocomplete";
 import DragPlan from "./DragPlan";
-import { useDispatch, useSelector } from "react-redux";
-import { useCookies } from "react-cookie";
-import AuthenticatedApp from "./AuthenticatedApp";
-import UnauthenticatedApp from "./UnauthenticatedApp";
 
 function App() {
-  const [cookies] = useCookies();
-
-  const isAuthenticated = cookies.isAuthenticated === "true";
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state);
-  const center = state.center;
-  // you will notice that i'm handling the constraints here not on my reducer
-  const handleIncrement = () =>
-    state.count < 10 && dispatch({ type: "INCREMENT" });
-  const handleDecrement = () =>
-    state.count > 0 && dispatch({ type: "DECREMENT" });
-  // const [center, setCenter] = useState({
-  //   lat: 40.7579746792255,
-  //   lng: -73.98546749996966,
-  // });
-
   return (
     <div className="center">
-      {isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />}
-
       <Router>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <PlacesAutoComplete
-                //should also comment the below
-                center={center}
-                // setCenter={setCenter}
-              />
-            }
-          />
-          <Route
-            path="/plan"
-            element={
-              <DragPlan
-                //should also comment the below
-                center={center}
-                // setCenter={setCenter}
-              />
-            }
-          />
+          <Route path="/" element={<PlacesAutoComplete />} />
+          <Route path="/plan" element={<DragPlan />} />
         </Routes>
       </Router>
     </div>
--- client/src/PlacesAutocomplete.jsx
+++ client/src/PlacesAutocomplete.jsx
@@ -8,17 +8,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchBusinesses } from "./redux/reducer.js";
 import Map from "./Map";
 import { MdLocationOff, MdLocationOn } from "react-icons/md";
-
+import Geocode from "react-geocode";
+Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAP_API_KEY);
+Geocode.enableDebug();
 const containerStyle = {
-  height: `50vh`,
+  height: `100vh`,
 };
 
-const PlacesAutoComplete = ({ center }) => {
+const PlacesAutoComplete = () => {
   const dispatch = useDispatch();
   const businesses = useSelector((state) => state.businesses);
-  // const center = state.center;
+  const center = useSelector((state) => state.center);
 
-  console.log("the buisnesses ", businesses);
+  // console.log("the buisnesses ", businesses);
   const [zoom, setZoom] = useState(10);
   const [value, setValue] = useState(null);
 
@@ -27,12 +29,10 @@ const PlacesAutoComplete = ({ center }) => {
   const resetMapCenter = (chosenLocation) => {
     const { lat, lng } = chosenLocation[0].geometry.location;
     const newCenter = { lat: lat(), lng: lng() };
-    // setCenter(newCenter);
     dispatch({
       type: "SET_CENTER",
       payload: { center: { lat: newCenter.lat, lng: newCenter.lng } },
     });
-
     setZoom(13);
     setUserCenter(newCenter);
   };
@@ -52,6 +52,43 @@ const PlacesAutoComplete = ({ center }) => {
     dispatch(fetchBusinesses(terms));
   };
 
+  console.log({ value });
+
+  const runGetLocation = () => {
+    console.log("getLocation run");
+    try {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        // setuserCoordinates({ latitude, longitude });
+        // setCenter({ lat: latitude, lng: longitude });
+
+        const newCenter = { lat: latitude, lng: longitude };
+        dispatch({
+          type: "SET_CENTER",
+          payload: { center: { lat: newCenter.lat, lng: newCenter.lng } },
+        });
+        setZoom(13);
+        setUserCenter(newCenter);
+
+        Geocode.fromLatLng(latitude, longitude).then(
+          (response) => {
+            const address = response.results[0].formatted_address;
+            if (address) {
+              // setPlace(address);
+              setValue(response.results[0]);
+
+              console.log({ address });
+            }
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
       <div style={{ position: "fixed", width: "100vw", bottom: 0 }}>
@@ -66,7 +103,7 @@ const PlacesAutoComplete = ({ center }) => {
             }}
             apiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}
           />
-          <div onClick={() => {}} className="icon">
+          <div className="icon" onClick={runGetLocation}>
             <span>
               <MdLocationOn
                 size={18}
