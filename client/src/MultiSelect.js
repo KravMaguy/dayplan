b372b798631e86e917660bb2812d6122eed7599c
krav maguy <flex4lease@gmail.com>
2022-04-01 16:54:08 -0500

    limit categories

--- client/src/MultiSelect.js
+++ client/src/MultiSelect.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import AsyncSelect from "react-select/async";
 import makeAnimated from "react-select/animated";
 import { useDispatch, useSelector } from "react-redux";
+import Login from "./Login";
 const animatedComponents = makeAnimated();
 const customStyles = {
   option: (provided, state) => ({
@@ -18,59 +19,76 @@ const customStyles = {
   },
 };
 
-const loadOptions = async (inputValue, { action }) => {
-  const options = [];
-  const { data } = await axios.get(`/autocomplete/${inputValue}`);
-  console.log(action, "is there an action ");
-  const myData = data.categories.map((category) =>
-    options.push({ value: category.alias, label: category.title })
-  );
-  const data2 = data.terms.map((term) =>
-    options.push({
-      value: term.text,
-      label: term.text,
-    })
-  );
-  return options.filter((i) =>
-    i.label.toLowerCase().includes(inputValue.toLowerCase())
-  );
-};
-
 export default function MultiSelectAsync() {
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.categories);
+  const checkLength = () => {
+    return categories.length < 2;
+  };
+  const loadOptions = async (inputValue, { action }) => {
+    const options = [];
+    const { data } = await axios.get(`/autocomplete/${inputValue}`);
+    const myData = data.categories.map((category) =>
+      options.push({ value: category.alias, label: category.title })
+    );
+    const data2 = data.terms.map((term) =>
+      options.push({
+        value: term.text,
+        label: term.text,
+      })
+    );
+    console.log(options, "in loadoptions we have");
+    return options.filter((i) =>
+      i.label.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  };
+
   const [inputValue, setInputValue] = useState("");
   const handleInputChange = (newValue) => {
-    console.log("handleinputChange ran");
     const inputValue = newValue.replace(/\W/g, "");
     setInputValue(newValue);
-    console.log(inputValue, "the input value");
     return inputValue;
   };
 
-  console.log(inputValue, "input values");
-  console.log(categories, "$$$$cateogries from redux");
   return (
-    <div>
-      <pre>inputValue: "{inputValue}"</pre>
-      <AsyncSelect
-        cacheOptions
-        defaultOptions
-        onInputChange={handleInputChange}
-        loadOptions={loadOptions}
-        components={animatedComponents}
-        isMulti
-        styles={customStyles}
-        onChange={(e) => {
-          dispatch({
-            type: "SET_CATEGORIES",
-            payload: e,
-          });
-          if (e.length > 2) {
-            return window.alert("reached ");
-          }
+    <>
+      <Login />
+      <div
+        className="homepage-main"
+        style={{
+          position: "relative",
+          top: "90px",
+          width: "90%",
+          margin: "0 auto",
         }}
-      />
-    </div>
+      >
+        <p>
+          Create a day plan based around popular activities, festivals, local
+          buisnesses, or whatever you feel like.
+        </p>
+        <button>Lets go</button>
+        <AsyncSelect
+          cacheOptions
+          defaultOptions
+          placeholder={"What do you feel like doing?"}
+          onInputChange={handleInputChange}
+          loadOptions={checkLength() && loadOptions}
+          components={animatedComponents}
+          isMulti
+          styles={customStyles}
+          onChange={(e) => {
+            dispatch({
+              type: "SET_CATEGORIES",
+              payload: e,
+            });
+          }}
+          noOptionsMessage={({ inputValue }) => {
+            return checkLength()
+              ? "no options found"
+              : "choose max 3 categories, remove an option or continue";
+          }}
+        />
+      </div>
+    </>
   );
 }
