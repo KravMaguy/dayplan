2993bcd9b79a59cbaa67b04ad3f592d6f560ac16
krav maguy <flex4lease@gmail.com>
2022-04-28 19:22:29 -0500

    fixed: You provided a `checked` prop to a form field without an `onChange` handler. This will render a read-only field.

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -14,32 +14,32 @@ const HomePage = () => {
           className="homepage-title"
           style={{ marginTop: "90px", marginLeft: "20px" }}
         >
-          <span class="homepage-title-inner">
+          <span className="homepage-title-inner">
             Create a day plan based around popular activities, festivals, local
             buisnesses, or whatever you feel like.{" "}
             <Link to={"/categories"}>Get Started</Link>
           </span>
         </h1>
-        {/* <div class="grid-container">
-          <div class="grid-item">
+        {/* <div className="grid-container">
+          <div className="grid-item">
             <img alt="redux" style={{ height: "100px" }} src={redux} />
           </div>
-          <div class="grid-item">
+          <div className="grid-item">
             <img alt="node" style={{ height: "100px" }} src={node} />
           </div>
 
-          <div class="grid-item">
+          <div className="grid-item">
             <img alt="npm" style={{ height: "100px" }} src={npm} />
           </div>
 
-          <div class="grid-item">
+          <div className="grid-item">
             <img
               alt="react"
               style={{ height: "100px", paddingTop: "5px" }}
               src={react}
             />
           </div>
-          <div class="grid-item last-double-columns">
+          <div className="grid-item last-double-columns">
             <img alt="mongodb" style={{ height: "100px" }} src={mongodb} />
           </div>
         </div> */}
