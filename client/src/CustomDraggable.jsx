3f903c8bdf808f4a9f5914398b49f531a3bfb61a
krav maguy <flex4lease@gmail.com>
2022-05-02 09:43:20 -0500

    fix heroku deploy useMediaQuery && refactor CustomDraggable

---
+++ client/src/CustomDraggable.jsx
@@ -0,0 +1,151 @@
+import { Draggable } from "react-beautiful-dnd";
+
+const grid = 8;
+const highlightedColor = "#8d7fbf";
+const getItemStyle = (isDragging, draggableStyle, currIdx, idx, collapsed) => ({
+  userSelect: "none",
+  cursor: "move",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+  background: isDragging
+    ? "lightgreen"
+    : currIdx === idx + 1
+    ? highlightedColor
+    : "grey",
+  ...draggableStyle,
+});
+
+const CustomDraggable = ({
+  location,
+  idx,
+  currIdx,
+  previous,
+  collapsed,
+  setCollapsed,
+  refProp,
+  handleSelectBox,
+}) => {
+  return (
+    <Draggable
+      className="draggable-element"
+      draggableId={location.id}
+      index={idx}
+    >
+      {(provided, snapshot) => {
+        if (idx + 1 === currIdx) {
+          refProp?.current?.scrollIntoView({
+            behavior: "smooth",
+            block: "nearest",
+          });
+        }
+        return (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            style={getItemStyle(
+              snapshot.isDragging,
+              provided.draggableProps.style,
+              currIdx,
+              idx,
+              collapsed
+            )}
+          >
+            <div
+              style={{
+                color: currIdx === idx + 1 ? "white" : "black",
+                textShadow:
+                  idx + 1 === currIdx ? "1px 1px 2px #000000" : "none",
+              }}
+              className="plan-card"
+              ref={refProp}
+            >
+              <div
+                className={
+                  idx + 1 === currIdx && currIdx !== collapsed
+                    ? "points-container seconddiv coolclass"
+                    : "seconddiv hidden"
+                }
+              >
+                <div className="numberCircle">
+                  {String.fromCharCode("A".charCodeAt(0) + idx)}
+                </div>
+                <div className="line"></div>
+                <div className="numberCircle">
+                  {String.fromCharCode("B".charCodeAt(0) + idx)}
+                </div>
+              </div>
+              <div className="locations">
+                <div
+                  className={
+                    idx + 1 === currIdx && currIdx !== collapsed
+                      ? "text top seconddiv coolclass"
+                      : "seconddiv"
+                  }
+                >
+                  <p>{previous ? previous.name : "Starting Location"}</p>
+                </div>
+                <div style={{ display: "flex" }}>
+                  <div className="text">
+                    <p>{location.name} </p>
+                    {}
+                    <p className="drag-address">
+                      {idx + 1 !== currIdx && (
+                        <a
+                          style={{ fontStyle: "italic" }}
+                          className=""
+                          href={`https://www.google.com/maps?q=${encodeURI(
+                            `${location.name} ${location?.address1} ${location?.city} ${location?.zip}`
+                          )}`}
+                          target="_blank"
+                        >
+                          {location?.address1}
+                        </a>
+                      )}
+                    </p>
+                  </div>
+
+                  {idx + 1 === currIdx && currIdx !== collapsed ? (
+                    <button
+                      className="no-link pure-material-button-text"
+                      style={{
+                        position: "absolute",
+                        right: 0,
+                        top: 0,
+                      }}
+                      onClick={() => setCollapsed(idx + 1)}
+                    >
+                      Close
+                    </button>
+                  ) : (
+                    <button
+                      className="no-link pure-material-button-text"
+                      style={{
+                        position: "absolute",
+                        right: 0,
+                        top: 0,
+                      }}
+                      onClick={() => handleSelectBox(idx + 1)}
+                    >
+                      Open
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div
+              className={
+                idx + 1 === currIdx && collapsed !== currIdx
+                  ? "mt-40"
+                  : "hidden"
+              }
+              id={`panel-${idx + 1}`}
+            />
+          </div>
+        );
+      }}
+    </Draggable>
+  );
+};
+
+export default CustomDraggable;
--- client/src/DragDropContent.jsx
+++ client/src/DragDropContent.jsx
@@ -1,21 +1,8 @@
 import { useState, useEffect, createRef } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-// import useMediaQuery from "./useMediaQuery";
-
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import useMediaQuery from "./useMediaQuery";
+import CustomDraggable from "./CustomDraggable";
 const grid = 8;
-const highlightedColor = "#8d7fbf";
-const getItemStyle = (isDragging, draggableStyle, currIdx, idx, collapsed) => ({
-  userSelect: "none",
-  cursor: "move",
-  padding: grid * 2,
-  margin: `0 0 ${grid}px 0`,
-  background: isDragging
-    ? "lightgreen"
-    : currIdx === idx + 1
-    ? highlightedColor
-    : "grey",
-  ...draggableStyle,
-});
 
 const getListStyle = (isDraggingOver) => ({
   background: isDraggingOver ? "lightblue" : "lightgrey",
@@ -30,19 +17,19 @@ const DragDropContent = ({
   setCollapsed,
   handleSelectBox,
 }) => {
-  // const matches = useMediaQuery("(min-width: 600px)");
+  const matches = useMediaQuery("(min-width: 600px)");
 
   const [elRefs, setElRefs] = useState([]);
 
   useEffect(() => {
-    if (derivedData.length > 0) {
+    if (derivedData.length > 0 && matches) {
       setElRefs((refs) =>
         Array(derivedData.length - 1)
           .fill()
           .map((_, i) => refs[i] || createRef())
       );
     }
-  }, [derivedData.length]);
+  }, [derivedData.length, matches]);
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
@@ -55,134 +42,17 @@ const DragDropContent = ({
             {derivedData.slice(1).map((location, idx, arr) => {
               let previous = arr[idx - 1];
               return (
-                <Draggable
-                  className="draggable-element"
+                <CustomDraggable
                   key={location.id}
-                  draggableId={location.id}
-                  index={idx}
-                >
-                  {(provided, snapshot) => {
-                    if (idx + 1 === currIdx) {
-                      elRefs[idx]?.current?.scrollIntoView({
-                        behavior: "smooth",
-                        block: "nearest",
-                      });
-                      elRefs[idx]?.current?.scrollIntoView({
-                        behavior: "smooth",
-                        block: "nearest",
-                      });
-                    }
-                    return (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        style={getItemStyle(
-                          snapshot.isDragging,
-                          provided.draggableProps.style,
-                          currIdx,
-                          idx,
-                          collapsed
-                        )}
-                      >
-                        <div
-                          style={{
-                            color: currIdx === idx + 1 ? "white" : "black",
-                            textShadow:
-                              idx + 1 === currIdx
-                                ? "1px 1px 2px #000000"
-                                : "none",
-                          }}
-                          className="plan-card"
-                          ref={elRefs[idx]}
-                        >
-                          <div
-                            className={
-                              idx + 1 === currIdx && currIdx !== collapsed
-                                ? "points-container seconddiv coolclass"
-                                : "seconddiv hidden"
-                            }
-                          >
-                            <div className="numberCircle">
-                              {String.fromCharCode("A".charCodeAt(0) + idx)}
-                            </div>
-                            <div className="line"></div>
-                            <div className="numberCircle">
-                              {String.fromCharCode("B".charCodeAt(0) + idx)}
-                            </div>
-                          </div>
-                          <div className="locations">
-                            <div
-                              className={
-                                idx + 1 === currIdx && currIdx !== collapsed
-                                  ? "text top seconddiv coolclass"
-                                  : "seconddiv"
-                              }
-                            >
-                              <p>
-                                {previous ? previous.name : "Starting Location"}
-                              </p>
-                            </div>
-                            <div style={{ display: "flex" }}>
-                              <div className="text">
-                                <p>{location.name} </p>
-                                {}
-                                <p className="drag-address">
-                                  {idx + 1 !== currIdx && (
-                                    <a
-                                      style={{ fontStyle: "italic" }}
-                                      className=""
-                                      href={`https://www.google.com/maps?q=${encodeURI(
-                                        `${location.name} ${location?.address1} ${location?.city} ${location?.zip}`
-                                      )}`}
-                                      target="_blank"
-                                    >
-                                      {location?.address1}
-                                    </a>
-                                  )}
-                                </p>
-                              </div>
-
-                              {idx + 1 === currIdx && currIdx !== collapsed ? (
-                                <button
-                                  className="no-link pure-material-button-text"
-                                  style={{
-                                    position: "absolute",
-                                    right: 0,
-                                    top: 0,
-                                  }}
-                                  onClick={() => setCollapsed(idx + 1)}
-                                >
-                                  Close
-                                </button>
-                              ) : (
-                                <button
-                                  className="no-link pure-material-button-text"
-                                  style={{
-                                    position: "absolute",
-                                    right: 0,
-                                    top: 0,
-                                  }}
-                                  onClick={() => handleSelectBox(idx + 1)}
-                                >
-                                  Open
-                                </button>
-                              )}
-                            </div>
-                          </div>
-                        </div>
-                        <div
-                          className={
-                            idx + 1 === currIdx && collapsed !== currIdx
-                              ? "mt-40"
-                              : "hidden"
-                          }
-                          id={`panel-${idx + 1}`}
-                        />
-                      </div>
-                    );
-                  }}
-                </Draggable>
+                  location={location}
+                  idx={idx}
+                  currIdx={currIdx}
+                  previous={previous}
+                  collapsed={collapsed}
+                  setCollapsed={setCollapsed}
+                  refProp={elRefs[idx]}
+                  handleSelectBox={handleSelectBox}
+                />
               );
             })}
 
